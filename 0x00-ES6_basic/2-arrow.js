98ef703f839d11c999b06e921069c3fa28bbc91c
Brian Omondi <omondiibrian00@gmail.com>
2024-01-03 10:22:54 +0300

    Working with es6 arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
-const getNeighborhoodsList = () => {
-    this.sanFranciscoNeighborhoods = ['SOMA','Union Square'];
+
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
     this.addNeighborhood = (newNeighborhood) => {
         this.sanFranciscoNeighborhoods.push(newNeighborhood);
         return this.sanFranciscoNeighborhoods;
     };
-};
-
-export default getNeighborhoodsList;
+}
