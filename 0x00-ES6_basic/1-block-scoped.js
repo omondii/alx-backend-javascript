78a5acacc136b87ae8a8e2ad13fb25ad6f2c760d
Brian Omondi <omondiibrian00@gmail.com>
2024-01-07 11:29:35 +0300

    update on block-scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 /* Difference btn const and let in es6 */
 export default function taskBlock (trueOrFalse) {
-  const task = false
-  const task2 = true
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true
-    const task2 = false
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2]
