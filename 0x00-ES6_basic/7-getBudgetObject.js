a4d09ea62636f1d2a25a3f68ebbc6051516ed8d5
Brian Omondi <omondiibrian00@gmail.com>
2024-01-07 12:44:13 +0300

    using es6 shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,5 @@
 /* Uses only keyname to store variables */
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-        income,
-        gdp,
-        capita
-    };
+    const budget = { income, gdp, capita };
     return budget;
 }
