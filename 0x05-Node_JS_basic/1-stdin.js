c0f5b2c7eefc4b42d6f8fb2caa124051ec3d4d0b
Brian Omondi <omondiibrian00@gmail.com>
2024-02-27 09:49:54 +0300

    Removed function

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,14 @@
 // Writes to stdin
-function stdin () {
     console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on("data", (data) => {
-    const name = data.toString().trim();
+    process.stdin.on("data", (data) => {
+        const name = data.toString().trim();
 
-    if (name) {
-        console.log(`Your name is: ${name}`)
-    } else {
-        console.log('Error');
-    }
-    console.log('This important software in now closing');
-    process.exit()
-});
-}
+        if (name) {
+            console.log(`Your name is: ${name}`)
+        } else {
+            console.log('Error');
+        }
+        console.log('This important software in now closing');
+        process.exit()
+    });
 
-module.exports = stdin;
