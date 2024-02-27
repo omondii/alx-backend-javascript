459e0e93526df1734f8e230937e8e2ec7b2c5a13
Brian Omondi <omondiibrian00@gmail.com>
2024-02-27 10:06:40 +0300

    Final ammend

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
 // Writes to stdin
-    console.log('Welcome to Holberton School, what is your name?');
-    process.stdin.on("data", (data) => {
-        const name = data.toString().trim();
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on("data", (data) => {
+    const name = data.toString().trim();
 
-        if (name) {
-            console.log(`Your name is: ${name}`)
-        } else {
-            console.log('Error');
-        }
-        console.log('This important software in now closing');
-        process.exit()
-    });
-
+    if (name) {
+        console.log(`Your name is: ${name}`)
+    } else {
+        console.log('Error');
+    }
+    console.log('This important software in now closing');
+    process.exit()
+});
--- 0x05-Node_JS_basic/test/1-stdin.test.js
+++ 0x05-Node_JS_basic/test/1-stdin.test.js
@@ -17,23 +17,25 @@ describe('stdin input output', () =>{
     test('outputs correct name', () => {
         const name = 'Brian';
         childProcess.stdin.write(name + '\n');
-        childProcess.stdout.on('data', (data) => {
+        childProcess.stdout.once('data', (data) => {
             const output = data.toString().trim();
             expect(output).toMatch(`Your name is: ${name}`);
+            done();
         });
     });
 
     test('Error when no name is provided', () => {
         childProcess.stdin.write('\n');
-        childProcess.stdout.on('data', (data) => {
+        childProcess.stdout.once('data', (data) => {
             const output = data.toString().trim();
             expect(output).toMatch('Error');
+            done();
         });
     });
 
     test('Exits after input', (done) => {
         childProcess.stdin.write('Anette\n');
-        childProcess.on('exit', () => {
+        childProcess.once('exit', () => {
             done();
         });
     });
