79cbb2de7d1d20d25b4068aa9893dd7894327182
Brian Omondi <omondiibrian00@gmail.com>
2024-02-29 20:43:04 +0300

    Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const assert = require('assert')
+const payment = require('./3-payment.js');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+    it('Stub calculateNumber to always return 10', () =>{
+        const calcStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        sendPaymentRequestToApi(100, 20);
+        expect(calcStub.calledWith('SUM', 100, 20)).to.be.true;
+        const calcSpy = sinon.spy(console, 'log');
+        expect(calcSpy.calledWith('The total is: 10')).to.be.false;
+    });
+});
