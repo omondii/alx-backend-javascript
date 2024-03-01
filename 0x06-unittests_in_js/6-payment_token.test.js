2193606cf540331e7df4aa0b1e39be5e51b4321a
Brian Omondi <omondiibrian00@gmail.com>
2024-03-01 07:51:10 +0300

    Ammend: Used done in 6test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -8,10 +8,12 @@ const getPaymentTokenFromApi = require('./6-payment_token.js')
 chai.use(require('chai-as-promised'));
 
 describe('Asynchronous for a test function', () => {
-    it('Resolve if success is true', () => {
-        expect(getPaymentTokenFromApi(true)).to.eventually.equal({data: 'Successful response from the API' })
+    it('Resolve if success is true', (done) => {
+        expect(getPaymentTokenFromApi(true)).to.eventually.equal({data: 'Successful response from the API' });
+        done()
     })
-    it('Reject if success is false', () => {
+    it('Reject if success is false', (done) => {
         expect(getPaymentTokenFromApi(false)).to.be.rejected;
+        done()
     });
 });
