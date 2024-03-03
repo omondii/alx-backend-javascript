06a299a4061171f085cdb45382044e992cbf87c1
Brian Omondi <omondiibrian00@gmail.com>
2024-03-03 15:54:07 +0300

    Ammend task 1.1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,10 @@
  */
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-  });
-  
-  process.stdin.on('end', () => {
-    console.log('This important software is now closing');
-  });
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
