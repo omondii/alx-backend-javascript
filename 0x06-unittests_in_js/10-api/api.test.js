564cdd9a4f190936e3ce5ea22d8090ebd1e16141
Brian Omondi <omondiibrian00@gmail.com>
2024-03-03 04:49:02 +0300

    More tests

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -17,6 +17,29 @@ describe('Tests for /', () => {
     });
 });
 
+
+describe('Tests for GET /cart/:id', () => {
+    var id = 12
+    it('Correct response when :id is  a number', (done) => {
+        request.get(`http://localhost:7865/cart/${id}`, (error, response, body) => {
+            if(response) {
+                expect(response.statusCode).to.be.equal(200);
+                expect(body).to.equals(`Payment methods for cart: ${id}`);
+                done()
+            }
+        });
+    });
+    it('Returns 404 when id isnot a number', (done) => {
+        id = 'n'
+        request.get('http://localhost:7865/cart/${id}', (error, response, body) => {
+            if (response) {
+                expect(response.statusCode).to.be.equal(404);
+                done();
+            }
+        })
+    })
+});
+
 describe('Tests for /available_payments', () => {
     it('Correct statusCode and response', (done) => {
         request.get(`https://localhost:7865/available_payments`, (error, response, body) => {
