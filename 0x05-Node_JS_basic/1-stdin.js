337fb5a629693b0e622a8292e3c6efca50d04d69
Brian Omondi <omondiibrian00@gmail.com>
2024-03-03 15:51:59 +0300

    Ammend task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 /**
  * Prompts user for name and displays the passed name
  */
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
     const name = data.toString().trim();
     console.log(`Your name is: ${name}`);
