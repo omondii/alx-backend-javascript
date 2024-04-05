cc467656468685462708d937ab42b41ef35bf95a
Brian Omondi <omondiibrian00@gmail.com>
2024-04-05 10:47:49 +0300

    Redid all tasks

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,5 @@
 export default class ClassRoom{
+    // Method which is run only once during the objects lifespan -> Setup
     constructor(maxStudentsSize){
         this._maxStudentsSize = maxStudentsSize;
     }
